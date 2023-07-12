1d510f718027fd83714d751fa2b9b8dbc9319c43
Fabio Massaioli <fabio.massaioli@gmail.com>
2023-07-12 13:16:15 +0200

    Output module from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,13 @@ import url from "url";
 
 export default {
     entry: './src/index.mjs',
+    experiments: {
+        outputModule: true,
+    },
     output: {
         path: url.fileURLToPath(new URL('dist/js', import.meta.url)),
-        filename: 'app.js'
+        filename: 'app.js',
+        module: true,
     },
     module: {
         rules: [
